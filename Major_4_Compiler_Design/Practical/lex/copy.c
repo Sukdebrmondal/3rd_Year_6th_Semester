76fa6a55ff4ebf0b378764a41be87f85d8a93a21
Sukdeb Mondal <sukdebdhalla@gmail.com>
2026-04-15 19:36:01 +0530

    lex

---
+++ Major_4_Compiler_Design/Practical/lex/copy.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    double a = 10.5;
+    double b = 20.3;
+    double sum;
+    sum = a + b;
+    printf("Sum = %f", sum);
+    return 0;
+}
---
+++ Major_4_Compiler_Design/Practical/lex/text.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main() {
+    float a = 10.5;
+    float b = 20.3;
+    float sum;
+    sum = a + b;
+    printf("Sum = %f", sum);
+    return 0;
+}
